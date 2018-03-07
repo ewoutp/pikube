81ec74247111ce904fd79c1980ddf82b56862e52
Ewout Prangsma <ewout@prangsma.net>
2018-03-07 11:48:04 +0100

    Fixed cat

--- setup_kubernetes_tools.sh
+++ setup_kubernetes_tools.sh
@@ -2,7 +2,7 @@
 
 sudo apt-get update && apt-get install -y apt-transport-https
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-sudo cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
+cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
 deb http://apt.kubernetes.io/ kubernetes-xenial main
 EOF
 sudo apt-get update
